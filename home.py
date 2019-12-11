50780806fb52e9f125fd10262bdf948a36079e4f
future-xy <31511840+future-xy@users.noreply.github.com>
2019-12-12 00:01:49 +0800

    Update home.py

--- home.py
+++ home.py
@@ -6,6 +6,7 @@
 # Mail    : fy38607203@163.com
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -19,9 +20,40 @@ def hello(name):
     return "Hello " + str(name)
 
 
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post {}'.format(post_id)
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return 'Subpath {}'.format(subpath)
+
+
 @app.route('/news')
 def news():
     return page
+
+
+@app.route('/')
+def home():
+    return """
+    Welcome!
+    Now, you can use: 
+        "/news"
+        "/user/..."
+        "/path/..."
+        "post/.."
+    """
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "USERNAME:" + request.form['username'] + "    " + "PASSWORD:" + request.form['password']
+    else:
+        print(request.args.get('name'))
+        return str(dir(request))
 
 
 if __name__ == '__main__':
