9e4ac70c4ad3500261bd151dce5d8a20bca60f10
future-xy <31511840+future-xy@users.noreply.github.com>
2020-01-30 11:33:05 +0800

    delete flaskenv

--- app/util/mail.py
+++ app/util/mail.py
@@ -9,16 +9,16 @@ from smtplib import *
 def sendMail(recv_addr, mail_content):
     server = 'smtp.sina.com'                # SMTP服务器
     send_addr = 'database_app@sina.com'     # 发送地址
-    #sender = '先背单词App  '               # 发件人
+    sender = '先背单词App  '                # 发件人
     pwd = 'ac3e268ebb74985e'                # SMTP授权码
 
-    #mail_title = '注册'
+    mail_title = '注册'
     smtp = SMTP_SSL(server)
     smtp.ehlo(server)
     smtp.login(send_addr, pwd)
 
     msg = MIMEText(mail_content, "plain", 'utf-8')      # 邮件内容
-    # msg["Subject"] = Header(mail_title, 'utf-8')      #邮件主题
+    msg["Subject"] = Header(mail_title, 'utf-8')      #邮件主题
     msg["From"] = send_addr  # 发送地址
     msg["To"] = Header(recv_addr, 'utf-8')              # 收件地址
 
