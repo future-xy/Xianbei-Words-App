def149f4a47f633a3d6f8e686137b3b3af4949d2
Zhao Yu <2723280065@qq.com>
2020-01-30 12:25:02 +0800

    Refactor

--- app/util/mail.py
+++ app/util/mail.py
@@ -11,12 +11,14 @@ def sendMail(recv_addr, mail_content, mail_title):
     server = 'smtp.sina.com'                            # SMTP服务器
     send_addr = 'database_app@sina.com'                 # 发送地址
     pwd = 'ac3e268ebb74985e'                            # SMTP授权码
+    h = Header('先背单词App', 'utf-8')
+    h.append('<database_app@sina.com>', 'ascii')
 
     msg = MIMEText(mail_content, "plain", 'utf-8')      # 邮件内容
 
     # 邮件头部
     msg['Subject'] = Header(mail_title, 'utf-8')        # 邮件主题
-    msg['From'] = send_addr                             # 发件人(因sina策略，发件人与发件邮箱必须保持一致)
+    msg['From'] = h                                     # 发件人
     msg['To'] = Header(recv_addr, 'utf-8')              # 收件人
 
     smtp = SMTP_SSL(server)
